18f536aa84242effc098c1032d1bce8ac34ad6a0
wonge1 <e.wong1@lancaster.ac.uk>
2023-11-24 20:08:01 +0000

    Added function to write by path

--- src/main/java/com/scc210groupproject/readwrite/FileSerializer.java
+++ src/main/java/com/scc210groupproject/readwrite/FileSerializer.java
@@ -2,9 +2,7 @@ package com.scc210groupproject.readwrite;
 
 import com.scc210groupproject.structure.Presentation;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
+import java.io.*;
 
 /**
  * @author wonge1
@@ -13,15 +11,30 @@ import java.io.ObjectOutputStream;
 public class FileSerializer {
 
     /**
+     * Write a presentation to a given path using
+     * @see FileSerializer#Serialize(FileOutputStream, Presentation)
+     * @param path file path to use
+     * @param presentation item to save to file
+     * @throws IOException thrown if FileOutputStream and/or ObjectOutputStream failed to start
+     * @throws FileNotFoundException thrown if path is a directory (or any other reason by)
+     * @see FileOutputStream
+     */
+    public static void WriteToPath(String path, Presentation presentation) throws IOException, FileNotFoundException {
+        try (FileOutputStream fileStream = new FileOutputStream(path)) {
+            Serialize(fileStream, presentation);
+        } //automatically closes stream
+    }
+
+    /**
      * Write a presentation to a FileOutputStream
      * @param fileStream stream to write to
      * @param presentation presentation to write
      * @throws IOException thrown if ObjectOutputStream failed to start
      */
     public static void Serialize(FileOutputStream fileStream, Presentation presentation) throws IOException {
-        ObjectOutputStream objectStream = new ObjectOutputStream(fileStream);
-        objectStream.writeObject(presentation);
-        objectStream.flush();
-        objectStream.close();
+        try (ObjectOutputStream objectStream = new ObjectOutputStream(fileStream)) {
+            objectStream.writeObject(presentation);
+            objectStream.flush();
+        } //automatically closes stream
     }
 }
