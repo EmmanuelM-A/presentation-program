c6d043ef6e60b743db545b069198b2111b3602d3
madukaag <madukaag@lancaster.ac.uk>
2023-12-02 00:05:29 +0000

    Added a colur scheme enum to store all the colour variables that will be used in the program

---
+++ src/main/java/com/scc210groupproject/applicationWIndow/ColourScheme.java
@@ -0,0 +1,14 @@
+package com.scc210groupproject.applicationWIndow;
+
+import java.awt.*;
+public enum ColourScheme {
+    BACKGROUND_COLOUR(new Color(211, 211, 211));
+    private final Color colour;
+    private ColourScheme(Color colour) {
+        this.colour = colour;
+    }
+
+    public Color getColour() {
+        return this.colour;
+    }
+}
