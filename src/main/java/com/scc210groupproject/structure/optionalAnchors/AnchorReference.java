4a5e7c5caeaf706bd4a126e2a323ddf944042592
wonge1 <e.wong1@lancaster.ac.uk>
2023-12-24 13:17:32 +0000

    remove redundant import

--- src/main/java/com/scc210groupproject/structure/optionalAnchors/AnchorReference.java
+++ src/main/java/com/scc210groupproject/structure/optionalAnchors/AnchorReference.java
@@ -5,7 +5,6 @@ import java.awt.geom.Point2D;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.io.ObjectStreamException;
 import java.io.Serializable;
 import java.util.ArrayList;
 
