69cceddd1fd5d133adf19660e9d8b148094a179d
wonge1 <e.wong1@lancaster.ac.uk>
2024-01-29 10:30:21 +0000

    added deprecation message to testui

---
+++ src/main/java/com/scc210groupproject/testui/package-info.java
@@ -0,0 +1,6 @@
+
+@java.lang.Deprecated
+/**
+* @deprecated This is a testui, this deprecation message is meant to distinguish classes in the testui and the actual ui
+*/
+package com.scc210groupproject.testui;
