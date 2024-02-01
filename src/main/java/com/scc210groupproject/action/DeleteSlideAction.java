aa107a37b12afab0157f99e07c96a192d281c340
wonge1 <e.wong1@lancaster.ac.uk>
2024-02-01 16:34:12 +0000

    prevent delete slide to delete last slide

--- src/main/java/com/scc210groupproject/action/DeleteSlideAction.java
+++ src/main/java/com/scc210groupproject/action/DeleteSlideAction.java
@@ -4,8 +4,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import com.scc210groupproject.structure.Presentation;
-import com.scc210groupproject.structure.Slide;
-import com.scc210groupproject.testui.MainDisplayPanel;
 import com.scc210groupproject.ui.SlideManager;
 
 public class DeleteSlideAction implements ActionListener {
@@ -13,7 +11,9 @@ public class DeleteSlideAction implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent _)
     {
-        Presentation.getOrCreate().removeSlide(SlideManager.instance.getCurrentSlide());
+        Presentation p = Presentation.getOrCreate();
+        if (p.getSlideCount() > 1)
+            p.removeSlide(SlideManager.instance.getCurrentSlide());
     }
 
 }
