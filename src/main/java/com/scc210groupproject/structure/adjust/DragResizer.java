83da78cf44b41fac585f6946129500452b7ca738
wonge1 <e.wong1@lancaster.ac.uk>
2024-02-06 10:39:42 +0000

    added multidrag

--- src/main/java/com/scc210groupproject/structure/adjust/DragResizer.java
+++ src/main/java/com/scc210groupproject/structure/adjust/DragResizer.java
@@ -3,7 +3,9 @@ package com.scc210groupproject.structure.adjust;
 import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.Point;
+import java.util.HashMap;
 
+import com.scc210groupproject.structure.BaseElement;
 import com.scc210groupproject.structure.helper.CoordinateUtils;
 import com.scc210groupproject.structure.input.InputEmulator.InputState;
 import com.scc210groupproject.structure.input.listeners.IMouseClicked;
@@ -13,29 +15,44 @@ import com.scc210groupproject.structure.input.listeners.IMouseExited;
 import com.scc210groupproject.structure.input.listeners.IMouseMoved;
 import com.scc210groupproject.structure.input.listeners.IMousePressed;
 import com.scc210groupproject.structure.input.listeners.IMouseReleased;
+import com.scc210groupproject.structure.input.listeners.IMultRelease;
+import com.scc210groupproject.structure.input.listeners.IMultSelect;
+import com.scc210groupproject.structure.input.listeners.IMultiDrag;
 import com.scc210groupproject.ui.MainDisplayPanel;
 
-public class DragResizer implements IMousePressed, IMouseReleased, IMouseMoved, IMouseDragged, IMouseEntered, IMouseExited, IMouseClicked {
+public class DragResizer implements IMousePressed, IMouseReleased, IMouseMoved, IMouseDragged, IMouseEntered, IMouseExited, IMouseClicked, IMultSelect, IMultRelease, IMultiDrag {
 
-    private Point last = null;
     private int operation = -1;
-    private SelectionBorder border = null;
+    private SelectionBorder border = new SelectionBorder();
+    private boolean inMultSelect = false;
+
+    public static HashMap<BaseElement, DragResizer> instances = new HashMap<>();
+
+    public void setMultiSelectMode(boolean multiSelect) {
+        if (multiSelect) {
+            border.setState(SelectionBorder.Mode.MOVEONLY);
+        }
+        else {
+            border.setState(SelectionBorder.Mode.FULL);
+        }
+        inMultSelect = multiSelect;
+    }
+
+    public DragResizer(BaseElement element) {
+        instances.put(element, this);
+    }
 
     @Override
     public void mousePressed(Object target, InputState state) {
-        System.out.println("hi");
         IResizable resizable = (IResizable)target;
         
         Point local = CoordinateUtils.convertSlideToLocalSpace(state.getLocationInSlide(), resizable.asBaseElement());
         operation = border.findPoint(local.x, local.y);
-        System.out.println(operation);
-        
     }
 
     @Override
     public void mouseReleased(Object target, InputState state) {
-
-        last = null;
+        // not used, here to block message being taken by another element
     }
 
     @Override
@@ -72,19 +89,20 @@ public class DragResizer implements IMousePressed, IMouseReleased, IMouseMoved, 
                 break;
             case 8:
                 MainDisplayPanel.instance.setCursor(new Cursor(Cursor.MOVE_CURSOR));
+                break;
+            default:
+                MainDisplayPanel.instance.setCursor(Cursor.getDefaultCursor());
+                break;
         }
     }
 
     @Override
     public void mouseDragged(Object target, InputState state) {
+        if (inMultSelect)
+            return;
+            
         IResizable resizable = (IResizable)target;
-        
-        Point global = state.getLocationInSlide();
-
-        int changeY = last != null ? global.y - last.y : 0;
-        int changeX = last != null ? global.x - last.x : 0;
-
-        last = global;
+        Dimension delta = state.getMouseDelta();
 
         Point oldPosition = resizable.getLocation();
         Dimension oldSize = resizable.getSize();
@@ -92,76 +110,102 @@ public class DragResizer implements IMousePressed, IMouseReleased, IMouseMoved, 
         switch (operation) {
 
             case 0:
-                if (oldSize.height - changeY > 20) {
-                    resizable.setLocation(new Point(oldPosition.x, oldPosition.y + changeY));
-                    resizable.setSize(new Dimension(oldSize.width, oldSize.height - changeY));
+                if (oldSize.height - delta.height > 20) {
+                    resizable.setLocation(new Point(oldPosition.x, oldPosition.y + delta.height));
+                    resizable.setSize(new Dimension(oldSize.width, oldSize.height - delta.height));
                 }
                 break;
             case 1:
-                if (oldSize.width + changeX > 20 && oldSize.height - changeY > 20) {
-                    resizable.setLocation(new Point(oldPosition.x, oldPosition.y + changeY));
-                    resizable.setSize(new Dimension(oldSize.width + changeX, oldSize.height - changeY));
+                if (oldSize.width + delta.width > 20 && oldSize.height - delta.height > 20) {
+                    resizable.setLocation(new Point(oldPosition.x, oldPosition.y + delta.height));
+                    resizable.setSize(new Dimension(oldSize.width + delta.width, oldSize.height - delta.height));
                 }
                 break;
             case 2:
-                if (oldSize.width + changeX > 20) {
+                if (oldSize.width + delta.width > 20) {
                     resizable.setLocation(new Point(oldPosition.x, oldPosition.y));
-                    resizable.setSize(new Dimension(oldSize.width + changeX, oldSize.height));
+                    resizable.setSize(new Dimension(oldSize.width + delta.width, oldSize.height));
                 }
                 break;
             case 3:
-                if (oldSize.width + changeX > 20 && oldSize.height + changeY > 20) {
+                if (oldSize.width + delta.width > 20 && oldSize.height + delta.height > 20) {
                     resizable.setLocation(new Point(oldPosition.x, oldPosition.y));
-                    resizable.setSize(new Dimension(oldSize.width + changeX, oldSize.height + changeY));
+                    resizable.setSize(new Dimension(oldSize.width + delta.width, oldSize.height + delta.height));
                 }
                 break;
             case 4:
-                if (oldSize.height + changeY > 20) {
+                if (oldSize.height + delta.height > 20) {
                     resizable.setLocation(new Point(oldPosition.x, oldPosition.y));
-                    resizable.setSize(new Dimension(oldSize.width, oldSize.height + changeY));
+                    resizable.setSize(new Dimension(oldSize.width, oldSize.height + delta.height));
                 }
                 break;
             case 5:
-                if (oldSize.width - changeX > 20 && oldSize.height + changeY > 20) {
-                    resizable.setLocation(new Point(oldPosition.x + changeX, oldPosition.y));
-                    resizable.setSize(new Dimension(oldSize.width - changeX, oldSize.height + changeY));
+                if (oldSize.width - delta.width > 20 && oldSize.height + delta.height > 20) {
+                    resizable.setLocation(new Point(oldPosition.x + delta.width, oldPosition.y));
+                    resizable.setSize(new Dimension(oldSize.width - delta.width, oldSize.height + delta.height));
                 }
                 break;
             case 6:
-                if (oldSize.width - changeX > 20) {
-                    resizable.setLocation(new Point(oldPosition.x + changeX, oldPosition.y));
-                    resizable.setSize(new Dimension(oldSize.width - changeX, oldSize.height));
+                if (oldSize.width - delta.width > 20) {
+                    resizable.setLocation(new Point(oldPosition.x + delta.width, oldPosition.y));
+                    resizable.setSize(new Dimension(oldSize.width - delta.width, oldSize.height));
                 }
                 break;
             case 7:
-                if (oldSize.width - changeX > 20 && oldSize.height - changeY > 20) {
-                    resizable.setLocation(new Point(oldPosition.x + changeX, oldPosition.y + changeY));
-                    resizable.setSize(new Dimension(oldSize.width - changeX, oldSize.height - changeY));
+                if (oldSize.width - delta.width > 20 && oldSize.height - delta.height > 20) {
+                    resizable.setLocation(new Point(oldPosition.x + delta.width, oldPosition.y + delta.height));
+                    resizable.setSize(new Dimension(oldSize.width - delta.width, oldSize.height - delta.height));
                 }
                 break;
             case 8:
-                resizable.setLocation(new Point(oldPosition.x + changeX, oldPosition.y + changeY));
+                resizable.setLocation(new Point(oldPosition.x + delta.width, oldPosition.y + delta.height));
         }
     }
 
     @Override
     public void mouseEntered(Object target, InputState state) {
+        if (inMultSelect)
+            return;
+            
         IResizable resizable = (IResizable)target;
-
-        last = null;
-        border = new SelectionBorder();
         resizable.setBorder(border);
     }
 
     @Override
     public void mouseExited(Object target, InputState state) {
-        IResizable resizable = (IResizable)target;
+        if (inMultSelect)
+            return;
 
+        IResizable resizable = (IResizable)target;
         resizable.setBorder(null);
     }
 
     @Override
     public void mouseClicked(Object target, InputState state) {
         // not used, here to block message being taken by another element
+    }
+
+    @Override
+    public void multiRelease(Object target, InputState state) {
+        IResizable resizable = (IResizable)target;
+        resizable.setBorder(null);
+        setMultiSelectMode(false);
+    }
+
+    @Override
+    public void multiSelect(Object target, InputState state) {
+        setMultiSelectMode(true);
+        IResizable resizable = (IResizable)target;
+        resizable.setBorder(border);
+    }
+
+    @Override
+    public void multiDrag(Object target, InputState state) {
+        IResizable resizable = (IResizable)target;
+        
+        Dimension delta = state.getMouseDelta();
+        Point oldPosition = resizable.getLocation();
+
+        resizable.setLocation(new Point(oldPosition.x + delta.width, oldPosition.y + delta.height));
     }
 }
---
+++ src/main/java/com/scc210groupproject/structure/input/listeners/IMultSelect.java
@@ -0,0 +1,7 @@
+package com.scc210groupproject.structure.input.listeners;
+
+import com.scc210groupproject.structure.input.InputEmulator.InputState;
+
+public interface IMultSelect {
+    public void multiSelect(Object target, InputState state);
+}
