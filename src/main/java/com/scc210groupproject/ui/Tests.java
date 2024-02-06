6028da087dd84af6a894bc819b9e56e09cb7541e
t.loxley <t.loxley@lancaster.ac.uk>
2024-02-06 14:15:23 +0000

    Commented out some code while implementing changes

--- src/main/java/com/scc210groupproject/ui/Tests.java
+++ src/main/java/com/scc210groupproject/ui/Tests.java
@@ -28,45 +28,45 @@ public class Tests extends JFrame {
         this.setResizable(true);
         this.setLayout(new GridBagLayout());
 
-        this.contextMenuPanel = new ContextMenuPanel(0, 0, Color.WHITE);
+        // this.contextMenuPanel = new ContextMenuPanel(0, 0, Color.WHITE);
 
-        this.menuBarTabs = new MenuBarTabs(this, this.contextMenuPanel, 0, 0, Color.WHITE);
+        // this.menuBarTabs = new MenuBarTabs(this, this.contextMenuPanel, 0, 0, Color.WHITE);
 
-        this.mainDisplayPanel = new MainDisplayPanel(0, 0, Color.WHITE);
+        // this.mainDisplayPanel = new MainDisplayPanel(0, 0, Color.WHITE);
 
-        this.slideManager = new SlideManager(this.mainDisplayPanel);
+        // this.slideManager = new SlideManager(this.mainDisplayPanel);
 
-        gbc.gridx = 0;
-        gbc.gridy = 0;
-        gbc.gridwidth = GridBagConstraints.REMAINDER;
-        gbc.fill = GridBagConstraints.BOTH;
-        gbc.weightx = 1.0;
-        gbc.weighty = 0.2;
-        gbc.insets = new Insets(0, 0, gap, 0);
-        this.add(menuBarTabs, gbc);
+        // gbc.gridx = 0;
+        // gbc.gridy = 0;
+        // gbc.gridwidth = GridBagConstraints.REMAINDER;
+        // gbc.fill = GridBagConstraints.BOTH;
+        // gbc.weightx = 1.0;
+        // gbc.weighty = 0.2;
+        // gbc.insets = new Insets(0, 0, gap, 0);
+        // this.add(menuBarTabs, gbc);
 
-        gbc.gridx = 0;
-        gbc.gridy = 1;
-        gbc.gridwidth = 1;
-        gbc.gridheight = 1;
-        gbc.weightx = 0.2;
-        gbc.weighty = 0.79;
-        gbc.insets = new Insets(gap, 0, gap, gap);
-        this.add(contextMenuPanel, gbc);
+        // gbc.gridx = 0;
+        // gbc.gridy = 1;
+        // gbc.gridwidth = 1;
+        // gbc.gridheight = 1;
+        // gbc.weightx = 0.2;
+        // gbc.weighty = 0.79;
+        // gbc.insets = new Insets(gap, 0, gap, gap);
+        // this.add(contextMenuPanel, gbc);
 
-        gbc.gridx = 1;
-        gbc.gridy = 1;
-        gbc.gridwidth = GridBagConstraints.REMAINDER;
-        gbc.weightx = 0.8;
-        gbc.insets = new Insets(gap, gap, gap, 0);
-        this.add(mainDisplayPanel, gbc);
+        // gbc.gridx = 1;
+        // gbc.gridy = 1;
+        // gbc.gridwidth = GridBagConstraints.REMAINDER;
+        // gbc.weightx = 0.8;
+        // gbc.insets = new Insets(gap, gap, gap, 0);
+        // this.add(mainDisplayPanel, gbc);
 
-        gbc.gridx = 0;
-        gbc.gridy = 2;
-        gbc.gridwidth = GridBagConstraints.REMAINDER;
-        gbc.weighty = 0.01;
-        gbc.insets = new Insets(gap, 0, 0, 0);
-        this.add(slideManager.createPresentationSlider(), gbc);
+        // gbc.gridx = 0;
+        // gbc.gridy = 2;
+        // gbc.gridwidth = GridBagConstraints.REMAINDER;
+        // gbc.weighty = 0.01;
+        // gbc.insets = new Insets(gap, 0, 0, 0);
+        // this.add(slideManager.createPresentationSlider(), gbc);
 
         setVisible(true);
 
@@ -86,32 +86,31 @@ public class Tests extends JFrame {
         - Sort out the slide scaling/resolution - set the slide size to match the size of the main display panel
      */
 
-    /*private void updateComponentSize()
-    {
-        int width = this.mainDisplayPanel.getWidth();
-        int height = this.mainDisplayPanel.getHeight();
+    // private void updateComponentSize()
+    // {
+    //     int width = this.mainDisplayPanel.getWidth();
+    //     int height = this.mainDisplayPanel.getHeight();
 
-        System.out.println("Main display Width: " + width + " Height: " + height);
+    //     System.out.println("Main display Width: " + width + " Height: " + height);
 
-        this.slideManager.getCurrentSlide().setDimension(new Dimension(width, height));
+    //     this.slideManager.getCurrentSlide().setDimension(new Dimension(width, height));
 
-        updateMainDisplaySlideSize(width, height);
-    }
+    //     updateMainDisplaySlideSize(width, height);
+    // }
 
-    private void updateMainDisplaySlideSize(int width, int height) {
-        //this.mainDisplayPanel.removeAll();
+    // private void updateMainDisplaySlideSize(int width, int height) {
+    //     //this.mainDisplayPanel.removeAll();
 
-        //this.slideManager.getCurrentSlide().setDiemension(new Dimension(width, height));
-        BufferedImage slide = this.slideManager.getCurrentSlide().createPreview(new Dimension(width, height));
+    //     //this.slideManager.getCurrentSlide().setDiemension(new Dimension(width, height));
+    //     BufferedImage slide = this.slideManager.getCurrentSlide().createPreview(new Dimension(width, height));
 
-        JLabel slideImage = new JLabel(new ImageIcon(slide));
-        this.mainDisplayPanel.add(slideImage);
-    }
+    //     JLabel slideImage = new JLabel(new ImageIcon(slide));
+    //     this.mainDisplayPanel.add(slideImage);
+    // }
 
-    public static void main(String args[]) {
-        new Tests();
-    }
-}
+    // public static void main(String args[]) {
+    //     new Tests();
+    // }
 
 
 
