2fab66f8d052c3842fb3eca8ad23f9decc275178
madukaag <madukaag@lancaster.ac.uk>
2024-02-10 20:42:03 +0000

    Working on the presentation mode #1

---
+++ src/main/java/com/scc210groupproject/ui/PresentationMode.java
@@ -0,0 +1,13 @@
+package com.scc210groupproject.ui;
+
+public class PresentationMode {
+    /*
+     * Open a new frame
+     * copy the functionality of the main display panel but have so it covers the whole screen instead
+     * each slide is assigned a list of actions
+     * actions are the events that happen to the slide like animations or transisitios
+     * next slide and previous slide buttons and arrow keys as well
+     * if there are no actions for a the current slide move onto the next
+     * 
+     */
+}
