801c840ab100d7a4ec75a034ea56c864f0ed50c6
t.loxley <t.loxley@lancaster.ac.uk>
2024-02-26 16:20:47 +0000

    closes #4, closes #12

--- src/main/java/com/scc210groupproject/App.java
+++ src/main/java/com/scc210groupproject/App.java
@@ -46,8 +46,6 @@ public class App {
     TODO LIST:
     - Do animations and transistions check discord
     - Settings windows
-    - drag feature to slides
     - shortcuts windows
     - presentation mode
-    - Color chooser dialog for text, shapes, arrow
 */
